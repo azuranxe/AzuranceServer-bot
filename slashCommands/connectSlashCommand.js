020b93d4d2016189451d724d70c0a44ec4f2c5b7
azuranxe <152092125+azuranxe@users.noreply.github.com>
2023-12-09 19:37:50 -0500

    Update connectSlashCommand.js

--- slashCommands/connectSlashCommand.js
+++ slashCommands/connectSlashCommand.js
@@ -2,6 +2,8 @@ const { SlashCommandBuilder, PermissionsBitField } = require('discord.js');
 const { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus, VoiceConnectionStatus } = require('@discordjs/voice');
 const { getRandomSongs, getTotalSongCount, insertCurrentSongMetadata } = require('/home/azurance/azurance-bot/utils/musicPlayer.js');
 const path = require('path');
+const {playMusic} = require('/home/azurance/azurance-bot/utils/playMusic.js')
+
 
 const playedSongs = new Set();
 
@@ -51,55 +53,5 @@ module.exports = {
 
     await interaction.reply({ content: `Đã kết nối với kênh <#${voiceChannelId}> và bắt đầu rung ~ cảm`, ephemeral: true });
 
-    async function playMusic(guildId, queue, db) {
-      const guildQueue = queue.get(guildId);
-      if (!guildQueue || guildQueue.songs.length === 0) {
-        if (playedSongs.size === await getTotalSongCount(db)) {
-            playedSongs.clear();
-            const newSongs = await getRandomSongs(db, await getTotalSongCount(db));
-            guildQueue.songs.push(...newSongs);
-        }
-        else if (guildQueue.voiceChannel) {
-          guildQueue.voiceChannel.leave()
-          queue.delete(guildId);
-          return;  
-        }
-      }
-
-      const songIndex = Math.floor(Math.random() * guildQueue.songs.length);
-      const song = guildQueue.songs.splice(songIndex, 1)[0];
-      if (song) {
-        playedSongs.add(song);
-      }
-
-      insertCurrentSongMetadata({
-        Title: song.title || 'Unknown Title',
-        Artist: song.artist || 'Unknown Artist',
-        Album: song.album || 'Unknown Album',
-        Genre: song.genre || 'Unknown Genre',
-        ReleaseYear: song.releaseYear || 'Unknown Year',
-        FilePath: song.filePath || 'Unknown FilePath',
-        CoverArtPath: song.coverArtPath || null
-      });
-
-      console.log(`Song:`, song.title)
-
-      if (!song.filePath) {
-        console.error('Song file path undefined.');
-        return;
-      }
-
-      const audioResource = createAudioResource(song.filePath);
-      const player = createAudioPlayer();
-
-      player.play(audioResource);
-      guildQueue.connection.subscribe(player);
-
-      player.on(AudioPlayerStatus.Idle, () => {
-        playMusic(guildId, queue, db);
-      });
-
-      player.on('error', error => console.error(`Error: ${error.message}`));
-    }
   },
 };
